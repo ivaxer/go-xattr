3eae6284586fd0f2720c25e1169d178bd60bee8e
John Khvatov <i@undev.ru>
2014-10-11 11:30:56 +0400

    expose syscalls wrappers to public interface
    Remove GetTo, which is replaced by Getxattr.

--- xattr.go
+++ xattr.go
@@ -39,12 +39,31 @@ func nullTermToStrings(buf []byte) (result []string) {
 	return
 }
 
+// Getxattr retrieves value of the extended attribute identified by attr
+// associated with given path in filesystem into buffer dest.
+//
+// On success, dest contains data associated with attr, retrieved value size sz
+// and nil error returned.
+//
+// On error, non-nil error returned. Getxattr returns error if dest was too
+// small for attribute value.
+//
+// A nil slice can be passed as dest to get current size of attribute value,
+// which can be used to estimate dest length for value associated with attr.
+//
+// Get is high-level function on top of Getxattr. Getxattr more efficient,
+// because it issues one syscall per call, doesn't allocate memory for
+// attribute data (caller can reuse buffer).
+func Getxattr(path, attr string, dest []byte) (sz int, err error) {
+	return get(path, attr, dest)
+}
+
 // Retrieves extended attribute data associated with path.
 func Get(path, attr string) ([]byte, error) {
 	attr = prefix + attr
 
 	// find size
-	size, err := get(path, attr, nil)
+	size, err := Getxattr(path, attr, nil)
 	if err != nil {
 		return nil, &XAttrError{"getxattr", path, attr, err}
 	}
@@ -54,31 +73,21 @@ func Get(path, attr string) ([]byte, error) {
 
 	// read into buffer of that size
 	buf := make([]byte, size)
-	size, err = get(path, attr, buf)
+	size, err = Getxattr(path, attr, buf)
 	if err != nil {
 		return nil, &XAttrError{"getxattr", path, attr, err}
 	}
 	return buf[:size], nil
 }
 
-// GetTo retrivies extended attribute data associated with path into dest. It
-// returns number of bytes retrived or non nil error.
-//
-// If attribute size is unknown caller should call GetTo with empty buffer or
-// guess it. If buffer is too short for value, GetTo returns error.
-//
-// GetTo is similar to Get but more efficient, because it issues one
-// getxattr(2) syscall per call, doesn't allocate memory for attribute data and
-// allows reuse buffer.
-func GetTo(path, attr string, dest []byte) (n int, err error) {
-	attr = prefix + attr
-	return get(path, attr, dest)
+func Listxattr(path string, dest []byte) (sz int, err error) {
+	return list(path, dest)
 }
 
 // Retrieves a list of names of extended attributes associated with path.
 func List(path string) ([]string, error) {
 	// find size
-	size, err := list(path, nil)
+	size, err := Listxattr(path, nil)
 	if err != nil {
 		return nil, &XAttrError{"listxattr", path, "", err}
 	}
@@ -88,27 +97,35 @@ func List(path string) ([]string, error) {
 
 	// read into buffer of that size
 	buf := make([]byte, size)
-	size, err = list(path, buf)
+	size, err = Listxattr(path, buf)
 	if err != nil {
 		return nil, &XAttrError{"listxattr", path, "", err}
 	}
 	return stripPrefix(nullTermToStrings(buf[:size])), nil
 }
 
+func Setxattr(path, attr string, data []byte, flags int) error {
+	return set(path, attr, data, flags)
+}
+
 // Associates data as an extended attribute of path.
 func Set(path, attr string, data []byte) error {
 	attr = prefix + attr
 
-	if err := set(path, attr, data, 0); err != nil {
+	if err := Setxattr(path, attr, data, 0); err != nil {
 		return &XAttrError{"setxattr", path, attr, err}
 	}
 	return nil
 }
 
+func Removexattr(path, attr string) error {
+	return remove(path, attr)
+}
+
 // Removes the extended attribute.
 func Remove(path, attr string) error {
 	attr = prefix + attr
-	if err := remove(path, attr); err != nil {
+	if err := Removexattr(path, attr); err != nil {
 		return &XAttrError{"removexattr", path, attr, err}
 	}
 	return nil
